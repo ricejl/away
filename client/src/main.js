cf9c118060a6efc8d4043afe33e07b360e615b58
LeviSmithCodes <eyevel@gmail.com>
2020-01-14 11:52:18 -0700

    Cleared out default vue junk

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
