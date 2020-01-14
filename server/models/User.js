24f95e937b453c4bbcd8a53c89968f4c56a08843
Jenny <ricejl@gmail.com>
2020-01-14 11:44:15 -0700

    edit user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,18 @@
-import mongoose from "mongoose"
-import bcrypt from 'bcryptjs'
-let Schema = mongoose.Schema
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+let Schema = mongoose.Schema;
 
-const User = new Schema({
-  name: { type: String, required: true },
-  //every email must be unique on the database
-  email: { type: String, required: true, unique: true },
-  hash: { type: String, required: true }
-}, { timestamps: true })
+const User = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    hash: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
 //schema.methods are used to add a method to a Model instance
-User.methods.validatePassword = function (password, hash) {
-  return bcrypt.compareSync(password, this.hash)
-}
+User.methods.validatePassword = function(password, hash) {
+  return bcrypt.compareSync(password, this.hash);
+};
 
-export default User
+export default User;
