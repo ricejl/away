a44f7f17ff36830fc542d6ee3e440c1b2e84ddcf
Jenny <ricejl@gmail.com>
2020-01-14 11:59:42 -0700

    add auth files and functions to client side

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import AuthService from "./AuthService";
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+async function init() {
+  let user = await AuthService.Authenticate();
+  if (user) {
+    store.commit("setUser", user);
+  } else {
+    router.push({ name: "login" });
+  }
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount("#app");
+}
+init();
