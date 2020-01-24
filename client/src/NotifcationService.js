3286ed2829fb895b9376d9c484069c9a23c68a19
Gerax Sotelo <gsotelo23@gmail.com>
2020-01-23 21:47:38 -0700

    Created modal for adding food items

--- client/src/NotifcationService.js
+++ client/src/NotifcationService.js
@@ -9,22 +9,22 @@ export default class NotificationService {
           `<form class="needs-validation profile-form" novalidate @change="checkStuff()">` +
           `<label class="mb-0">First Name <span style="color: red">*</span></label>` +
           `<input id="firstName" class="swal2-input" value="${profileData.firstName ||
-            ""}" placeholder="Enter your first name">` +
+          ""}" placeholder="Enter your first name">` +
           `<label class="mb-0">Last Name <span style="color: red">*</span></label>` +
           `<input id="lastName" class="swal2-input" value="${profileData.lastName ||
-            ""}" placeholder="Enter your last name">` +
+          ""}" placeholder="Enter your last name">` +
           `<label class="mb-0">Username <span style="color: red">*</span></label>` +
           `<input id="username" class="swal2-input" value="${profileData.username ||
-            ""}" placeholder="Enter a display name">` +
+          ""}" placeholder="Enter a display name">` +
           `<label class="mb-0">Favorite quote</label>` +
           `<input id="description" class="swal2-input" value="${profileData.description ||
-            ""}" placeholder="Enter favorite quote">` +
+          ""}" placeholder="Enter favorite quote">` +
           `<label class="mb-0">Email <span style="color: red">*</span></label>` +
           `<input id="email" type="email" class="swal2-input" value="${profileData.email ||
-            ""}" placeholder="Enter public email address">` +
+          ""}" placeholder="Enter public email address">` +
           `<label class="mb-0">Image URL</label>` +
           `<input id="img-url" type="text" class="swal2-input" value="${profileData.imgURL ||
-            ""}" placeholder="Enter image URL">` +
+          ""}" placeholder="Enter image URL">` +
           `<span style="color: red">*</span> indicates required field` +
           `</form>`,
         // NOTE for email validation look into validitystate=true, @change
@@ -64,6 +64,43 @@ export default class NotificationService {
       }
     } catch (error) {
       return false;
+    }
+  }
+
+  static async inputFood(title = "Enter Inputs", mealData) {
+    try {
+      const { value: formValues } = await Swal.fire({
+        title,
+        html:
+          `<form class="meal-form"">` +
+          `<label class="mb-0">Food Item<span style="color: red">*</span></label>` +
+          `<input id="food-name" class="swal2-input" value="${mealData.foodItems.foodName ||
+          ""}" placeholder="Item name">` +
+          `<label class="mb-0">Details</label>` +
+          `<input id="food-details" class="swal2-input" value="${mealData.foodItems.details ||
+          ""}" placeholder="Details">` +
+          `</form>`,
+        focusConfirm: false,
+        preConfirm: () => {
+          let foodName = document.getElementById("food-name").value;
+          let foodDetails = document.getElementById("food-details").value;
+          if (!foodName) {
+            Swal.showValidationMessage("Please fill out required fields");
+          }
+          return [
+            foodName,
+            foodDetails
+          ];
+        }
+      });
+      if (formValues) {
+        return {
+          foodName: formValues[0],
+          details: formValues[1],
+        };
+      }
+    } catch (error) {
+      return error;
     }
   }
 
